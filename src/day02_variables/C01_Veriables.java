21b04737a6dc74127739181d748400127d1db07a
ilker61 <jakyuer@gmail.com>
2022-07-19 14:09:09 +0200

    ikinci gun versiyonu

---
+++ src/day02_variables/C01_Veriables.java
@@ -0,0 +1,28 @@
+package day02_variables;
+
+public class C01_Veriables {
+    public static void main(String[] args) {
+        System.out.println("Hallo word yazdirana ne mutlu");
+
+        int level=1;
+        System.out.println(level);
+
+        boolean ogrenciMi=true;
+        boolean salakMi=true;
+
+        char ozelSembol='#';
+        char harf='K';
+
+        String ad, soyad, sehir;
+        ad="Necmi";
+        soyad="Yildiz";
+        sehir="Mugla";
+
+        // Tek deklerasyon ile birden cok deger deklare edilebilir. Deklare edildikten sonra tekr teker
+        // atamalari yapilir.
+
+        int lev=20, yas=98, bas=780;  // seklinde de yapililir.
+
+
+    }
+}
